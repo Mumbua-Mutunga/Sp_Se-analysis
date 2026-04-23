237aef0e94787994ecf8d48cb1a68d1f44e54a32
Katie Hampson <katiehampson@gmail.com>
2026-04-23 23:45:13 +0100

    updated figures
    revised plots for clinical signs and test results

---
+++ KatieNotes.R
@@ -0,0 +1,21 @@
+intro = 900
+methods  = 100 + 450 + 275 + 600; methods
+results = 300 + 180 + 220 + 100; results
+discussion = 1028
+
+total = intro + methods + results + discussion; total
+
+
+inc = 1.16 / 1000000 
+period = 84/12
+(inc / period)* 1000
+
+
+pop <- 47000000
+HDR <- 10
+dogs <- pop / HDR; dogs
+inc <- 0.3 / 1000
+tests <- dogs * inc
+tests
+RDT <- 10
+RDT * tests
